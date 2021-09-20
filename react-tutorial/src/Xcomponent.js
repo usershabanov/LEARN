dafffb727f6fc6e672aef8d43aa1d237c1038aab
usershabanov <orhan.shabanov@internet.ru>
2021-09-20 19:37:21 +0300

    the first reaction on react

---
+++ react-tutorial/src/Xcomponent.js
@@ -0,0 +1,23 @@
+import  './X.css'
+
+
+
+
+
+function Xcomponent() {
+    let x;
+    x = "Меня зовут Василий Пупкин и я будущий программист";
+    console.log(x);
+    return(
+        <div>
+            чекни consolelog
+        </div>
+    )
+   }
+
+
+
+      
+    
+
+export default Xcomponent;
